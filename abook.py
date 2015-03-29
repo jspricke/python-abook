7272470df5cf43d723936daf392c7e3f21abdb27
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2015-03-29 22:35:12 +0200

    Remove empty lines before writing abook file
    This fixes 5ee3578, so the file does not accumulate new lines.

--- abook.py
+++ abook.py
@@ -252,6 +252,7 @@ class Abook(object):
         filename = book.filename
         book.filename = None
         entries = book.write()
+        entries = [e for e in entries if e is not '']
         entries = [e.replace(' = ', '=', 1) for e in entries]
         entries.append('\n')
         content = '\n'.join(entries)
