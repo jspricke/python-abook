9c4742ee3e82c30c0323df2e25d4dad8b179ba1c
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2016-07-29 11:16:57 +0200

    Version 0.4.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='abook',
-      version='0.3.0',
+      version='0.4.0',
       description='''
        Python library to convert between Abook and vCard
        ''',
