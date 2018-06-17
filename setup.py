7ef85e59b067ce78462ffbee2766d957bda7ae15
Jochen Sprickerhof <git@jochen.sprickerhof.de>
2018-06-17 12:47:19 +0200

    version 0.5.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='abook',
-      version='0.5.2',
+      version='0.5.3',
       description='Python library to convert between Abook and vCard',
       long_description=open('README.rst').read(),
       author='Jochen Sprickerhof',
